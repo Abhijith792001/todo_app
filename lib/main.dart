6b563aef8e03b8af64baafa9a4e2f85a5920ceaf
Abhijitj792001 <abhijithjeejamon792001@gmail.com>
2025-04-15 10:56:31 +0530

    model created

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_ , child) {
+        return GetMaterialApp(
+          
+        );
+      }
+    );
+  }
+}
---
+++ lib/model/todo_model.dart
@@ -0,0 +1,28 @@
+class TodoModel {
+  int? id;
+  String? title, description, priority;
+  bool? completed;
+  DateTime? dueDate;
+
+  TodoModel({
+    this.id,
+    this.title,
+    this.description,
+    this.priority,
+    this.completed,
+    this.dueDate,
+  });
+  factory TodoModel.fromJson(Map<String, dynamic> json) {
+    return TodoModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      completed: json['completed'],
+      dueDate: json['dueDate'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'title': title, 'description': description,'completed':completed,'dueDate':dueDate};
+  }
+}

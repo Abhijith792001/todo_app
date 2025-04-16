2c2f380aede16f410c918db28cf01e436ca4d364
Abhijitj792001 <abhijithjeejamon792001@gmail.com>
2025-04-16 08:34:04 +0530

    Home page header ui is finished

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -52,7 +52,7 @@ class HomeScreen extends StatelessWidget {
           Column(
             children: [
               Container(
-                padding: EdgeInsets.all(16),
+                padding: EdgeInsets.all(16.sp),
                 width: Get.width,
                 height: Get.height * .4,
                 decoration: BoxDecoration(
@@ -63,35 +63,103 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 child: SafeArea(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  child: Column(
                     children: [
-                     Row(
-                      
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                         Container(
-                          margin:EdgeInsets.only(top: 6),
-                          height: 25.h,
-                          child: CircleAvatar()),
-                      Column(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                            Text('Hello Abhi',style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15
-                            ),),
-                            Text('How are today ?..'
-                            ,style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 11,)
-                            )
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(height: 25.h, child: CircleAvatar()),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Hello Abhi..',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15.sp,
+                                      fontFamily: 'ADLaM',
+                                    ),
+                                  ),
+                                  Text(
+                                    'How are today ?..',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 11.sp,
+                                      fontFamily: 'ADLaM',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            spacing: 10.sp,
+                            children: [
+                              Image.asset(
+                                'assets/images/calendar.png',
+                                height: 16.h,
+                                width: 16.w,
+                              ),
+                              Text(
+                                'Jan 05 2025',
+                                style: TextStyle(
+                                  fontFamily: 'ADLaM',
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
-                      ],
-                     ),
-                     Icon(LucideIcons.calendar)
+                      Row(
+                        children: [
+                          Container(
+                            // color: Colors.blue,
+                            child: Image.asset(
+                              'assets/images/bg-img.png',
+                              height: 120.h,
+                              width: 160.w,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              // color: Colors.amber,
+                              child: Column(
+                                spacing: 10,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    'Take Control of \nYour day',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontFamily: 'PermanentMarker',
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Turn your goals into daily victories with \nour simple and powerful to-do app',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      fontFamily: 'ADLaM',
+                                      color: Colors.white,
+                                      fontSize: 9.5,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
@@ -101,19 +169,32 @@ class HomeScreen extends StatelessWidget {
           Positioned(
             bottom: 0,
             child: Container(
+              padding: EdgeInsets.all(16),
               width: Get.width,
-              height: 500.h,
+              height: 460.h,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(62),
-                  topRight: Radius.circular(62)
+                  topRight: Radius.circular(62),
                 ),
                 gradient: LinearGradient(
-                  
                   begin: Alignment(0.50, -0.00),
                   end: Alignment(0.50, 1.00),
                   colors: [const Color(0xFFD7E2E6), Colors.white],
                 ),
+              ),
+              child: Column(
+             
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 10.h),
+                    child: Text(
+                      'Recent Task',
+                      style: TextStyle(fontFamily: 'ADLaM', color: Colors.black,fontSize: 24),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
